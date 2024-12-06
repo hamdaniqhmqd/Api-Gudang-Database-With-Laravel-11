85f7759b2c0f3af635e9d595542b4a7b55281594
ahmad-hamdani <ahmadhamdaniiii2020@gmail.com>
2024-12-06 07:43:27 +0700

    penambahan fitur rahasia

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/symlink', function () {
+    Artisan::call('storage:link');
+});
+Route::get('/fresh_data', function () {
+    Artisan::call('migrate:fresh');
+});
+Route::get('/migrate_data', function () {
+    Artisan::call('migrate');
 });
